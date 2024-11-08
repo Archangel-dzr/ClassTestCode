5ce00d79cb3ef7882aab319269ffc3a810ebde16
Archangel <781446156@qq.com>
2024-11-08 19:07:49 +0800

    完善内容

--- .vscode/Person.cpp
+++ .vscode/Person.cpp
@@ -15,7 +15,7 @@
  * @
  * @Author: Archangel 781446156@qq.com
  * @Date: 2024-11-08 18:06:06
- * @LastEditTime: 2024-11-08 18:14:49
+ * @LastEditTime: 2024-11-08 19:06:41
  * @LastEditors: Archangel 781446156@qq.com
  * @Description: 设计一个人的类（基类），再分别设计一个学生类（子类）和教师类（子类）单继承人类
  * @FilePath: \ClassTestCode\.vscode\Person.cpp
@@ -23,7 +23,7 @@
  */
 
 #include <iostream>
-#include <stdexcept> 
+#include <stdexcept>
 using namespace std;
 
 class Person {
@@ -39,12 +39,17 @@ public:
         if (gender != 'M' && gender != 'F') throw invalid_argument("性别必须为'M'或'F'");
     }
 
+    // 输出个人信息
+    virtual void printInfo() const {
+        cout << "姓名: " << name << ", 年龄: " << age << ", 性别: " << (gender == 'M' ? "男" : "女") << endl;
+    }
+
     void eat() const {
-        cout << "Person " << name << " is eating." << endl;
+        cout << name << " is eating." << endl;
     }
 
     void sleep() const {
-        cout << "Person " << name << " is sleeping." << endl;
+        cout << name << " is sleeping." << endl;
     }
 };
 
@@ -57,6 +62,12 @@ public:
     Student(const string &name, int age, char gender, int score, const string &id)
         : Person(name, age, gender), score(score), id(id) {
         if (score < 0 || score > 100) throw invalid_argument("分数必须在0到100之间");
+    }
+
+    // 输出学生信息
+    void printInfo() const override {
+        Person::printInfo();
+        cout << "学号: " << id << ", 分数: " << score << endl;
     }
 
     void play() const {
@@ -79,6 +90,12 @@ public:
         if (years < 0) throw invalid_argument("教龄不能为负值");
     }
 
+    // 输出教师信息
+    void printInfo() const override {
+        Person::printInfo();
+        cout << "教龄: " << years << "年, 职业: " << job << endl;
+    }
+
     void teach() const {
         cout << "Teacher " << job << " is teaching." << endl;
     }
@@ -91,16 +108,19 @@ public:
 int main() {
     try {
         Person p("Archangel", 24, 'M');
+        p.printInfo(); // 输出个人信息
         p.eat();
         p.sleep();
 
         Student s("Tom", 22, 'M', 90, "2019001");
+        s.printInfo(); // 输出学生信息
         s.eat();
         s.sleep();
         s.play();
         s.study();
 
-        Teacher t("Jane", 30, 'F', 5, "Teacher");
+        Teacher t("Jane", 30, 'F', 5, "Math Teacher");
+        t.printInfo(); // 输出教师信息
         t.eat();
         t.sleep();
         t.teach();
@@ -111,3 +131,4 @@ int main() {
 
     return 0;
 }
+
